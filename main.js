3c98e10f493b2d340020546479cb3739defd46ac
Gaia Cravesana <gcravesana@gmail.com>
2021-04-28 12:08:58 +0100

    added typewriter effect

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const intro = document.getElementById('intro');
 const portfolio = document.getElementById('portfolio');
 const start = document.getElementById('start');
 
+//show portfolio
 const portfolioShow = () => {
+  intro.style.opacity='0';
   intro.style.display ='none';
   portfolio.style.display ='block';
 }
 
 start.onclick = portfolioShow;
+
+
